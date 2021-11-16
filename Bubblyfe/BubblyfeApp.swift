9c4a6360e17ff99472c29c6546d33d13ffeb67e2
orleonedev <orleone.dev@gmail.com>
2021-11-16 12:13:21 +0100

    Initial Commit

---
+++ Bubblyfe/BubblyfeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblyfeApp.swift
+//  Bubblyfe
+//
+//  Created by Oreste Leone on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BubblyfeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

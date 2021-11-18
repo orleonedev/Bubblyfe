4d22dc7120351a6c4145e1963b811335cffd342d
JasmineAufiero <jasmineaufiero@gmail.com>
2021-11-18 12:09:24 +0100

    Termine Lista in Add Bubbles

--- Bubblyfe/AddBubbles.swift
+++ Bubblyfe/AddBubbles.swift
@@ -19,7 +19,8 @@ struct AddBubbles: View {
         var body: some View {
         NavigationView {
             
-            VStack {
+            VStack(alignment: .leading) {
+                
                 Toggle(isOn: $checkfamily) { // Toggle per Family
                     Image(systemName: "heart.circle.fill")
                         .resizable()
@@ -32,6 +33,8 @@ struct AddBubbles: View {
                         .foregroundColor(.blue)
                         
                 } // END Toggle Family
+                
+                Divider()
                 
                 Toggle(isOn: $checkStudy) { // Toggle per Study
                     Image(systemName: "books.vertical.circle.fill")
@@ -46,6 +49,8 @@ struct AddBubbles: View {
                         
                 } // END Toggle Free Time
                 
+                Divider()
+                
                 Toggle(isOn: $checkFreeTime) { // Toggle per Study
                     Image(systemName: "calendar.circle.fill")
                         .resizable()
@@ -59,28 +64,43 @@ struct AddBubbles: View {
                         
                 } // END Toggle Free Time
                 
+                Divider()
+                
                 
                 Toggle(isOn: $checkFriends) { // Toggle per Friends
-                    Image(systemName: "heart.circle.fill")
+                    Image(systemName: "person.2.circle.fill")
                         .resizable()
-                        .foregroundColor(Color.init(red: 255/255, green: 207/255, blue: 0))
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.805, brightness: 0.998, opacity: 0.752))
                         .frame(width: 28, height: 28, alignment: .center)
                     
-                    Text("Free Time")
+                    Text("Friends")
                         .bold()
                         .fontWeight(.light)
-                        .foregroundColor(Color.init(red: 255/255, green: 207/255, blue: 0))
-                        
-                } // END Toggle Free Time
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.805, brightness: 0.998, opacity: 0.752))
+                } // END Toggle Friends
                 
                 
+                Divider()
                 
+                Toggle(isOn: $checkExtra) { // Toggle per Extra
+                    Image(systemName: "person.2.circle.fill")
+                        .resizable()
+                        .foregroundColor(Color(red: 100/255, green: 234/255, blue: 0/255))
+                        .frame(width: 28, height: 28, alignment: .center)
+                    
+                    Text("Extra")
+                        .bold()
+                        .fontWeight(.light)
+                        .foregroundColor(Color(red: 100/255, green: 234/255, blue: 0/255))
+                } // END Toggle Extra
                 
-                
+                Divider()
                 
                 
                 
             }.padding()
+            .frame(width: 400, height: 300, alignment: .top)
+            .offset(x: 0, y: -200)
             
             .navigationTitle("Add Bubbles")
             .navigationBarTitleDisplayMode(.inline)

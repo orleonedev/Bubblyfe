7f0acd257c5316cad95ca37997c8993a65e3aeb2
orleonedev <orleone.dev@gmail.com>
2021-11-18 17:26:47 +0100

    more

--- Bubblyfe/View/OnBoarding/OnBoardingNoBubbles.swift
+++ Bubblyfe/View/OnBoarding/OnBoardingNoBubbles.swift
@@ -63,6 +63,8 @@ struct OnBoardingNoBubbles: View {
 
 struct OnBoardingNoBubbles_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView{
         OnBoardingNoBubbles()
+        }
     }
 }
--- Bubblyfe/View/OnBoarding/OnBoardingSelectionView.swift
+++ Bubblyfe/View/OnBoarding/OnBoardingSelectionView.swift
@@ -10,11 +10,16 @@ import SwiftUI
 struct OnBoardingSelectionView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationTitle("Choose Bubbles")
+            .navigationBarTitleDisplayMode(.inline)
+//            .navigationBarItems(trailing: )
     }
 }
 
 struct OnBoardingSelection_Previews: PreviewProvider {
     static var previews: some View {
-        OnBoardingSelectionView()
+        NavigationView{
+            OnBoardingSelectionView()
+        }
     }
 }

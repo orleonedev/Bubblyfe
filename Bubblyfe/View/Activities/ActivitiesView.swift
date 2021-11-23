4a148190faa2208b0d9ce0fd63291990dec1d29b
JasmineAufiero <jasmineaufiero@gmail.com>
2021-11-23 11:42:57 +0100

    jjjjjj

--- Bubblyfe/View/Activities/ActivitiesView.swift
+++ Bubblyfe/View/Activities/ActivitiesView.swift
@@ -11,8 +11,8 @@ struct ActivitiesView: View {
     @EnvironmentObject var bubbleStore: BubblesStore
     @EnvironmentObject var activitiesStore: ActivityStore
     @State var areCompleted: Bool = false
-//    @State var selectedActivity: Activity
-
+    @State var showActivityDetailModal: Bool = false // per passare alla schermata della singola attività
+    
     let columns = [
         GridItem(.flexible()),
         GridItem(.flexible()),
@@ -21,37 +21,45 @@ struct ActivitiesView: View {
     var body: some View {
         
         
-            ScrollView {
-                Picker("Activity Status", selection: $areCompleted) {
-                    Text("Ongoing").tag(false)
-                    Text("Completed").tag(true)
-                }.pickerStyle(.segmented)
-                    .padding()
-                
-                    LazyVGrid(columns: columns, spacing: 8) {
-                        ForEach(activitiesStore.activities.filter{ return $0.isCompleted == areCompleted }, id: \.self) { activity in
-                            VStack(alignment: .leading){
-                                HStack(alignment: .center, spacing: 8){
-                                    Image(systemName: activity.icon)
-                                    Text(activity.title)
-                                        .font(.headline)
-                                        .lineLimit(1)
-                                }
-                                Text(activity.details)
-                                    .font(.subheadline)
-                                    .fontWeight(.light)
-                                    .lineLimit(2)
-                                
-                            }.padding()
-                                .background(RoundedRectangle(cornerRadius: 20).foregroundColor(activity.cardColor))
-                        }
-                    }
-                    .padding(.horizontal)
-                }
+        ScrollView {
+            Picker("Activity Status", selection: $areCompleted) {
+                Text("Ongoing").tag(false)
+                Text("Completed").tag(true)
+            }.pickerStyle(.segmented)
+                .padding()
             
-        
+            LazyVGrid(columns: columns, spacing: 8) {
+                ForEach(activitiesStore.activities.filter{ return $0.isCompleted == areCompleted }, id: \.self) { activity in
+                    
+                    VStack(alignment: .leading){
+                        HStack(alignment: .center, spacing: 8){
+                            Image(systemName: activity.icon)
+                            Text(activity.title)
+                                .font(.headline)
+                                .lineLimit(1)
+                        }
+                        Text(activity.details)
+                            .font(.subheadline)
+                            .fontWeight(.light)
+                            .lineLimit(2)
+                        
+                    }.padding()
+                        .background(RoundedRectangle(cornerRadius: 20).foregroundColor(activity.cardColor))
+                    
+                        .onTapGesture {
+                            showActivityDetailModal.toggle()
+                        }.sheet(isPresented: $showActivityDetailModal, content: {
+                            ActivityDetail(showActivityDetailModal: $showActivityDetailModal, selectedActivity: activity).environmentObject(activitiesStore)
+                        })
+                }
+            }
+        }
+        .padding(.horizontal)
     }
+    
+    
 }
+
 
 struct ActivitiesView_Previews: PreviewProvider {
     static var previews: some View {
